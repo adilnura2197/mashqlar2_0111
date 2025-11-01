ffdf31839f2112501686759067ad2afb9c0699bc
adilnura2197 <adilnura2197@gmail.com>
2025-11-01 17:41:30 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# #1-misol
+# c = 0
+# for i in range(25, 1500):
+#     if i % 15 == 0 or i % 14 == 0 and i % 105 != 0:
+#         c += 1
+#
+# print(c)
+
+
+# #2-misol
+# c = 0
+# for i in range(1, 2000):
+#     if i % 6 != 0 and i % 2 == 0 and i % 9 == 0:
+#         c += 1
+#
+# print(c)
+
+
+# #3-misol
+# c = 0
+# for i in range(100, 2500):
+#     if i % 11 == 0 and i % 5 != 0 or i % 7 != 0:
+#         c += 1
+#
+# print(c)
+
+
+# #4-misol
+# c = 0
+# for i in range(1, 1800):
+#     if i % 20 == 0 and i % 4 == 0 and i % 10 == 0:
+#         c += 1
+#
+# print(c)
+
+
+# #5-misol
+# c = 0
+# for i in range(40, 3000):
+#     if i % 18 == 0 and i % 15 != 0 and i % 90 == 0:
+#         c+= 1
+# 
+# print(c)
